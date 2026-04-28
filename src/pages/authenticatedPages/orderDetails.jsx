c3981daad4433ef1e01491ee1c78ebf63448928e
Victor <victorokeke89@gmail.com>
2026-04-28 17:04:56 +0100

    default layout error

--- src/pages/authenticatedPages/orderDetails.jsx
+++ src/pages/authenticatedPages/orderDetails.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import DefaultLayout from "../../layouts/DefaultLayout";
+import DefaultLayout from "../../layouts/defaultLayout";
 import {
   Avatar,
   Button,
