3751f31d564d9ad1d92ec1fff704c69453c74fc9
Victor <victorokeke89@gmail.com>
2026-04-28 15:29:10 +0100

    update faq error fixes

--- src/pages/onboard/Helps.jsx
+++ src/pages/onboard/Helps.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet-async";
-import Faq from "../../components/molecule/faq/Faq";
-import OnboardLayout from "../../layouts/OnboardLayout";
+import Faq from "../../components/molecule/faq/faq";
+import OnboardLayout from "../../layouts/onboardLayout";
 
 const Helps = () => {
   return (
