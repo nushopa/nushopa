5670f93bbc9135e55c689a9982803fb2f3967f9c
Victor <victorokeke89@gmail.com>
2026-04-28 13:40:21 +0100

    contact error fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import Abouts from "./pages/onboard/Abouts.jsx";
 import Store from "./pages/onboard/Store.jsx";
 import Helps from "./pages/onboard/Helps.jsx";
-import Contact from "./pages/onboard/Contact.jsx";
+import Contact from "./pages/onboard/contact.jsx";
 import Logistics from "./pages/onboard/Logistics.jsx";
 import GuestLanding from "./pages/GuestLanding.jsx";
 
