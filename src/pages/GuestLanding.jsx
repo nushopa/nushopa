9ef8d2dfb47e4b79734d8de98bd362bd2cefeb22
Victor <victorokeke89@gmail.com>
2026-04-28 15:36:46 +0100

    store error fixes

--- src/pages/GuestLanding.jsx
+++ src/pages/GuestLanding.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import Store from "./onboard/Store";
+import Store from "./onboard/store";
 import Landing from "./landing";
 
 const GuestLanding = () => {
