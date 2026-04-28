573e25c98263a3c9b3457daa4512aafc2c05f667
Victor <victorokeke89@gmail.com>
2026-04-28 15:23:30 +0100

    onbaord error on about page

--- src/pages/onboard/Abouts.jsx
+++ src/pages/onboard/Abouts.jsx
@@ -1,10 +1,10 @@
-import OnboardLayout from "../../layouts/OnboardLayout";
+import OnboardLayout from "../../layouts/onboardLayout";
 import { Helmet } from "react-helmet-async";
-import Testimonial from "../../components/testimonial/Testimonial";
+import Testimonial from "../../components/testimonial/testimonial";
 import Company from "./component/Company";
 import Story from "./component/Story";
 import Choose from "./component/Choose";
-import { FooterWithSitemap } from "../../components/common/footer/Footer";
+import { FooterWithSitemap } from "../../components/common/footer/footer";
 
 const Abouts = () => {
   return (
