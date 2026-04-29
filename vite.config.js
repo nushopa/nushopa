3c1dcb643c398068fecc837e93780846be2fba48
Victor <victorokeke89@gmail.com>
2026-04-29 07:04:03 +0100

    resolve error logo

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [react(), visualizer()],
   resolve: {
-    caseSensitiveMatch: true, 
+    caseSensitiveMatch: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks: (id) => {
           if (id.includes("node_modules"))
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
           if (id.includes("src/components/common")) return "common";
           if (id.includes("src/components/pages")) return "pages";
           if (id.includes("src/components/landingSection")) return "landing";
